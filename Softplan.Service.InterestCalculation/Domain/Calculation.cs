832a6044ee8139ba0782b8bd1e56ae563cd10453
Alexandre Lorenzon <alexandre.lorenzon@outlook.com>
2019-11-24 19:47:07 -0300

    Criação dos testes unitários

--- Softplan.Service.InterestCalculation/Domain/Calculation.cs
+++ Softplan.Service.InterestCalculation/Domain/Calculation.cs
@@ -11,18 +11,37 @@ namespace Softplan.Service.InterestCalculation.Domain
 
         public Calculation(decimal initialContribution, decimal interestRate, int months)
         {
+
+            #region Initial contribution validations
+
+            if (initialContribution < 0)
+                throw new SoftException(ErrorCodes.InvalidValue, "Valor inicial não pode ser negativo");
+
             var initialContributionIsValid = double.TryParse(initialContribution.ToString(), out double initialValue);
 
             if (!initialContributionIsValid)
-                throw new SoftException(ErrorCodes.InvalidValue, "Valor inicial informado é inválido");
+                throw new SoftException(ErrorCodes.InvalidFormat, "Valor inicial informado é inválido");
+
+            #endregion
+
+            #region Interest rate validations
+
+            if (interestRate < 0)
+                throw new SoftException(ErrorCodes.InvalidValue, "Taxa de juros não pode ser negativo");
 
             var interestRateIsValid = double.TryParse(interestRate.ToString(), out double interestRateValue);
 
             if (!interestRateIsValid)
-                throw new SoftException(ErrorCodes.InvalidValue, "Taxa de juros informada é inválido");
+                throw new SoftException(ErrorCodes.InvalidFormat, "Taxa de juros informada é inválido");
+
+            #endregion
+
+            #region Months validations
 
             if (months < 0)
-                throw new SoftException(ErrorCodes.InvalidValue, "Número de meses informado é inválido");
+                throw new SoftException(ErrorCodes.InvalidValue, "Número de meses não pode ser negativo");
+
+            #endregion
 
             InitialContribution = initialValue;
             InterestRate = interestRateValue;
